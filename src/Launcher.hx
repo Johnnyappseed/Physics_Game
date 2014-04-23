b85a407177a6fcc6ca19c850e6b2f8f809e7b1fe
Johnnyappseed <HaynesJ@asmsa.org>
2014-04-23 12:03:08 -0500

    basics

--- src/Launcher.hx
+++ src/Launcher.hx
@@ -14,6 +14,7 @@ import box2D.dynamics.B2FixtureDef;
 import box2D.dynamics.B2World;
 import box2D.dynamics.joints.B2RevoluteJointDef;
 import box2D.dynamics.joints.B2RevoluteJoint;
+//http://www.emanueleferonato.com/2009/10/05/basic-box2d-rope/
 
 /**
  * ...
@@ -28,19 +29,17 @@ class Launcher extends Sprite
 	public function new(x:Int, y:Int, radius:Int) 
 	{
 		super();
-		staticCircle = Main.game.createCircle (x, y, 20, false);
-		log = Main.game.createBox (x - 50, y, 300, 30, true, 1.0);
+		staticCircle = Main.game.createCircle (x, y, 15, false);
+		log = Main.game.createBox (x - 35, y+35, 300, 20, true, 1.0);
+		log.setAngle(-0.7853981633974483);
 		var jointLog:B2RevoluteJointDef = Main.game.revoluteJointFunction(staticCircle, log, staticCircle.getWorldCenter());
 		Main.game.World.createJoint(jointLog);
-		wieght = Main.game.createBox(x + 50, y + 50, 50, 50, true, 2.0);
+		wieght = Main.game.createBox(x + 35, y, 45, 40, true, 30.0);
 		var joint:B2Vec2; 
-		joint = new B2Vec2((x+50) * Main.PHYSICS_SCALE,y * Main.PHYSICS_SCALE);
+		joint = new B2Vec2((x+35) * Main.PHYSICS_SCALE,(y-35) * Main.PHYSICS_SCALE);
 		var jointWieght:B2RevoluteJointDef = Main.game.revoluteJointFunction(log, wieght, joint);
 		Main.game.World.createJoint(jointWieght);
-		//log.setAngle(-45);
 		
-		
-		
-	}
+		}
 	
 }
