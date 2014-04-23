55049cf4dcdf742beaafdd8e97d35149560454c5
Johnnyappseed <HaynesJ@asmsa.org>
2014-04-23 10:54:01 -0500

    public create box change

--- src/Launcher.hx
+++ src/Launcher.hx
@@ -29,13 +29,13 @@ class Launcher extends Sprite
 	{
 		super();
 		staticCircle = Main.game.createCircle (x, y, 20, false);
-		log = Main.game.createBox (x - 50, y, 300, 30, false, 1.0);
+		log = Main.game.createBox (x - 50, y, 300, 30, true, 1.0);
 		var jointLog:B2RevoluteJointDef = Main.game.revoluteJointFunction(staticCircle, log, staticCircle.getWorldCenter());
 		Main.game.World.createJoint(jointLog);
-		wieght = Main.game.createBox(x + 50, y + 50, 50, 50, true, 1.0);
+		wieght = Main.game.createBox(x + 50, y + 50, 50, 50, true, 10.0);
 		var joint:B2Vec2; 
-		joint = new B2Vec2(x+50,y);
-		var jointWieght:B2RevoluteJointDef = Main.game.revoluteJointFunction(log, wieght, log.getWorldPoint(joint));//i think i need to apply the world physics scale to this
+		joint = new B2Vec2((x+50) * Main.PHYSICS_SCALE,y * Main.PHYSICS_SCALE);
+		var jointWieght:B2RevoluteJointDef = Main.game.revoluteJointFunction(log, wieght, joint);
 		Main.game.World.createJoint(jointWieght);
 		//log.setAngle(-45);
 		
