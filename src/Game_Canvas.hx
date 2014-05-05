9c727ff1c5bdaa22e30b23969213fbd6074210f3
Johnnyappseed <HaynesJ@asmsa.org>
2014-05-05 02:38:43 -0500

    Launcher now dynamicly scales... everything else fixed for the most part.... not including graphics that is

--- src/Game_Canvas.hx
+++ src/Game_Canvas.hx
@@ -106,7 +106,7 @@ class Game_Canvas extends Sprite
 			else 
 			{
 				catapultDestroy();
-				catapult = new Launcher(400, 300);
+				catapult = new Launcher(400, 350);
 				this.addChild(catapult);
 				fired = false;
 				lookingAtLauncher = true;
@@ -137,7 +137,7 @@ class Game_Canvas extends Sprite
 	
 	public function creation()
 	{
-		catapult = new Launcher(400, 300);
+		catapult = new Launcher(400, 350);
 		this.addChild(catapult);
 		destroyAmmo();
 		castle = new Castle_1();
