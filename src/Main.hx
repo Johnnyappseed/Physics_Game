16d8b1abda382627e4e973f78a7162b2b377d28f
mosest <mosest@asmsa.org>
2014-05-05 15:45:03 -0500

    have trees and clouds yay and music YAY

--- src/Main.hx
+++ src/Main.hx
@@ -19,6 +19,8 @@ import openfl.Assets;
 import flash.events.Event;
 import flash.events.MouseEvent;
 import motion.Actuate;
+import flash.media.Sound;
+import flash.media.SoundChannel;
 
 /**
  * ...
@@ -42,6 +44,11 @@ class Main extends Sprite
 	var startMenu:Sprite;
 	var playButton:Sprite;
 	
+	//sound
+	var bgmusic:Sound;
+	var jumpmusic:Sound;
+	var sc:SoundChannel;
+	
 	//screen positioning var'sss
 	var xv:Float;
 	var yv:Float;
@@ -63,6 +70,10 @@ class Main extends Sprite
 		PhysicsDebug = new Sprite ();
 		xv = 0.0;
 		yv = 0.0;
+		
+		//music
+		bgmusic = Assets.getSound("sound/bgmusic.wav");
+		bgmusic.play(0,9999999);
 		
 		addChild (PhysicsDebug);
 		World = new B2World(new B2Vec2 (0, 10.0), true);
